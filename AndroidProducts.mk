adfa5133dfa01971e15a27a62eb981036917b722
Stealth1226 <rishawniyer06@gmail.com>
2022-05-11 19:48:13 +0530

    miatoll: import product makefiles

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aospa_miatoll.mk
+
+COMMON_LUNCH_CHOICES := \
+    aospa_miatoll-userdebug
